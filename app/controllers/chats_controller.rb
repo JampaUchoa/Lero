111c58fdee22c5dcde8e26e47189a64d048ee6ba
Joao Uchoa <joaopedrouchoa@hotmail.com>
2015-11-28 01:57:25 -0300

    visual improvments

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,67 @@
+class ChatsController < ApplicationController
+
+    def sendmsg
+      room_id = (params[:roomId])
+      @message = (params[:message])
+      Chat.create(message: @message, room_id: room_id)
+      render json: {}
+    end
+
+=begin
+    def receive
+      if logged_in
+        lastmsg = (params[:id]).to_i
+        usertenancy = current_user.tenancies.pluck(:room_id)
+        if usertenancy.any?
+          @newchats = Chat.where('id > ?', lastmsg).where(room_id: usertenancy).last(30)
+          @chatmsgs = Array.new
+          @newchats.each do |m|
+            @chatmsgs << {
+              id: m.id,
+              message: markdown(m.message),
+              room: m.room_id,
+              created_at: m.created_at.utc.to_i*1000,
+              userid: m.user.username,
+              username: m.user.name,
+              photo:
+              if m.user.photo?
+                m.user.photo.to_s
+              else
+                ActionController::Base.helpers.asset_path('user-default.png')
+              end
+            }
+          end
+        end
+        render json: @chatmsgs
+      end
+    end
+
+
+
+
+
+
+
+      def markdown(text)
+        options = {
+          filter_html:     true,
+          no_images:       true,
+          hard_wrap:       true,
+          link_attributes: { rel: 'nofollow', target: "_blank" },
+          space_after_headers: true,
+          fenced_code_blocks: true
+        }
+
+        extensions = {
+          autolink:           true,
+          superscript:        true,
+          disable_indented_code_blocks: true
+        }
+
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+      markdown.render(text).html_safe
+      end
+
+end
