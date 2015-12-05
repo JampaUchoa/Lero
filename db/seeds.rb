424b6b025c3a376790d73a9c8fc46027a8c65f05
Joao Uchoa <joaopedrouchoa@hotmail.com>
2015-12-05 02:02:59 -0300

    login welcome

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Chat.delete_all
-Room.delete_all
-Tenant.delete_all
-User.delete_all
-
 Room.create(name: "Lero")
