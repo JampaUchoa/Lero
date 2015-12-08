6d1ced5bd56169eceb26919bee41457115ecbede
Joao Uchoa <joaopedrouchoa@hotmail.com>
2015-12-08 03:26:58 -0300

    only active tenancies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
 
   def chat
-    @joined = current_user.tenancies
+    @joined = current_user.tenancies.where(active: true)
     @popular_rooms = Room.all.order("tenants_count DESC").limit(12)
   end
 
